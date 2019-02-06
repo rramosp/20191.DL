94474fb9e5f68fdfe370d8647bc4ad8b7d59ad80
jdariasl <judaarlo@gmail.com>
2019-02-05 22:51:39 -0500

    U4.1 avance

---
+++ libraries/DataPreparationRNN.py
@@ -0,0 +1,14 @@
+import numpy as np
+#split a univariate sequence into samples
+def split_sequence(sequence, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		end_ix = i + n_steps
+		if end_ix > len(sequence)-1:
+			break
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return np.array(X), np.array(y)
+
+
